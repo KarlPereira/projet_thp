03eba3cc66d482c28c244bf71285cb0b895f6a39
Karl Pereira <karlpereira14@gmail.com>
2017-09-07 20:28:33 +0000

    Création model Review

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   belongs_to :user
   has_many :photos
   has_many :reservations
+  has_many :reviews
   geocoded_by :address
 
 after_validation :geocode, if: :address_changed?
@@ -21,5 +22,11 @@ validates :summary, presence: true, length: {maximum: 600}
 validates :address, presence: true
 
 validates :price, numericality: { only_integer: true, greater_than: 1 } #seulement des nombres entiers supérieurs à 1
+
+
+def average_rating
+      reviews.count == 0 ? 0 : reviews.average(:star).round(2)
+
+ end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,6 @@ validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 has_many :rooms
 has_many :reservations
+has_many :reviews
 end
 
